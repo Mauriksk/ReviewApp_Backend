4b98ffbf5f87dbf754ef32cb60e0e07d3302319f
Mauriksk <otarksk@gmail.com>
2024-11-03 13:00:17 +0100

    Se solicita imagen en el backend para evitar fujas de seguridad en el front

--- src/requests/getRestaurants.js
+++ src/requests/getRestaurants.js
@@ -8,8 +8,19 @@ const getRestaurants = async (city, limit) => {
     
     // Verifica si `limit` está definido y es un número válido, luego aplica el límite
     const limitedResults = limit ? searchData.results.slice(0, limit) : searchData.results;
+
+     // Mapeo para agregar la URL de la imagen a cada atracción
+     const enrichedResults = limitedResults.map(restaurant => {
+        const photo = restaurant.photos && restaurant.photos.length > 0 ? restaurant.photos[0] : null;
+        const imageUrl = photo ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photo.photo_reference}&key=${apiKey}` : null;
+
+        return {
+            ...restaurant,
+            imageUrl // Agrega la URL de la imagen al objeto
+        };
+    });
     
-    return limitedResults;
+    return enrichedResults;
 }
 
   module.exports = {
