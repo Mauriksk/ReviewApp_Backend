06dea131595438765837539ef63c7474761cbbdb
Mauriksk <otarksk@gmail.com>
2024-11-03 12:23:32 +0100

    Se agregar cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const app = express()
 const { getRestaurants } = require("./requests/getRestaurants")
+const cors = require('cors');
 
 app.listen(3000, async () => {
-    const restaurants = await getRestaurants("Lepzig", )
-    console.log("running 3000 port", restaurants)
+    console.log("running 3000 port")
 })
+
+app.use(cors());
 
 app.get("/api/restaurants", async (req, res) => {
     const restaurants = await getRestaurants("Lepzig", 20)
